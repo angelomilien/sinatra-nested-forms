a74b4efc45c089a4d5e933c0dfc7638eb630940b
angelo milien <angelomilienjoseph@gmail.com>
2021-05-06 15:23:48 -0400

    done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+    
+    attr_accessor :name, :height, :weight
+
+    @@all = []
+
+    def initialize(prop)
+      @name = prop[:name]
+      @weight = prop[:weight]
+      @height = prop[:height]
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+    
+    attr_accessor :name, :type, :booty
+    
+    @@all = []
+
+    def initialize(prop)
+       @name = prop[:name]
+       @type = prop[:type]
+       @booty = prop[:booty]
+       @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.clear
+      @@all = []
+    end
+    
 end
